b1e44c1fe9950b69ca9e2d194e3ce4bf1ac776c2
Puiu Dumitru99 <puiudumitru99@gmail.com>
2023-05-19 15:21:22 +0300

    Create cordonation

--- script.js
+++ script.js
@@ -3,42 +3,50 @@ class pointer
     constructor(punct)
     {
         this.punct=document.createElement(punct.tag);
-        this.punct.style.top=punct.localStorage;
+        this.punct.style.top=punct.top;
         this.punct.style.left=punct.left;
         this.punct.classList=punct.clas;
         document.querySelector('.display').append(this.punct);     
     }
 }
-let x,y,score=0;
-let date=[localStorage.getItem('x'),localStorage.getItem('y')];
-function create_CORDONATION()
+    let x,y;
+    let cordonation=
+    {   
+        clas:"point",
+        tag:"div",
+        top:"0",
+        left:"0"
+    }
+ function create_CORDONATION()
 {
-    localStorage.setItem('x',Math.random()*100);
-    localStorage.setItem('y',Math.random()*100);
-    console.log(date)
+    localStorage.setItem('L',Math.random()*100);
+    localStorage.setItem('T',Math.random()*100);
+    cordonation.top=JSON.parse(localStorage.getItem('T'))+'%';
+    cordonation.left=JSON.parse(localStorage.getItem('L'))+'%';
+    console.log(cordonation)
 }
-function del()
-{
-    date.shift();
-    date.pop();
-}
-
-let cordonation=
-{   
-    clas:"pi",
-    tag:"div",
-    top:localStorage.getItem('x')+"%",
-    left:localStorage.getItem('y')+"%"
-}
-$(document).on('click','.pi',function()
-{
-    create_CORDONATION()
-    create_point();
-})
 
 function create_point()
 {     
+    create_CORDONATION();
+    start();
     new pointer(cordonation);
-    create_CORDONATION()
 }
+
+function start()
+{
+    timeR=date=new Date().getTime();
+    let Timex=parseInt(timeR/1000%60)+10;
+    localStorage.setItem('startTime',Timex);
+    
+}
+
+$(document).on('click','.point',function()
+{    
+
+    create_point();
+    create_CORDONATION();  
+    $('.point').first().remove();
+})
+
 

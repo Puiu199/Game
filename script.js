42cded82cfca4a57428efcaafeac77a5e3cff1e4
Puiu Dumitru99 <puiudumitru99@gmail.com>
2023-05-25 08:49:00 +0300

    V2.0

--- script.js
+++ script.js
@@ -12,96 +12,107 @@ class pointer
         document.querySelector('.display').append(this.punct);     
     }
 }    
-    let cordonation=
-        {   
-            clas:"point",
-            tag:"div",
-            top:"0",
-            left:"0"
-        }
-    let life=
-        {
-            clas:"heart",
-            tag:"span",
-            tC:""
-        }
-    let cancel=
-        {
-            clas:"heart"
-        }
-        life._proto_=cordonation;
-        cancel._proto_=life;
+let cordonation=
+    {   
+        clas:"point",
+        tag:"div",
+        top:"0",
+        left:"0"
+    }
+let life=
+    {
+        clas:"heart",
+        tag:"span",
+        tC:""
+    }
+let cancel=
+    {
+        clas:"cancel"
+    }
+    life._proto_=cordonation;
+    cancel._proto_=life;
 
-    function life_1()
-        {
-            new pointer(life);
+function life_1()
+    {
+        new pointer(life);
+    }
+    function can_cel()
+    {
+        new pointer(cancel);
+    }
+    $(document).on('click','.cancel',function()
+    {
+        $('.cancel').first().remove();
+        $(this).remove();
+    })
+$(document).on('click','.heart',function()
+    {           
+        if(i===0)
+        { 
+            console.warn("Finish");
+            $(this).hide();     
         }
-        $(document).on('click','.cancel',function()
+        else if(i<=5)
         {
-            
-        })
-    $(document).on('click','.heart',function()
-        {           
-            if(i===0)
-            { 
-                console.warn("Finish");
-                $(this).hide();     
-            }
-            else if(i<=5)
-            {
-                life.tC="+5";
-                i=i+5;
-                $(this).hide();
-                create_CORDONATION_life();     
-            }
-            else if(i>=7)
-            {
-                life.tC="+3";
-                i=i+3;
-                $(this).hide();
-                create_CORDONATION_life(); 
-            }
-            else
-            {
-                $(this).hide();
-                console.warn('Game over O_O');
-                create_CORDONATION_life(); 
-            }
-        })
-    function create_CORDONATION_life()
-        {
-            life.top=JSON.parse(localStorage.getItem('L'))+'%';
-            life.left=JSON.parse(localStorage.getItem('T'))+'%';
-            s=localStorage.getItem('L')*99;
-            console.log(s);
-            setTimeout(()=>{           
-            life_1();
-            },s )    
+            life.tC="+5";
+            i=i+5;
+            $(this).hide();
+            create_CORDONATION_life();     
         }
+        else if(i>=7)
+        {
+            life.tC="+3";
+            i=i+3;
+            $(this).hide();
+            create_CORDONATION_life(); 
+        }
+        else
+        {
+            $(this).hide();
+            console.warn('Game over O_O');
+            create_CORDONATION_life(); 
+        }
+    })
+function create_CORDONATION_life()
+    {
+        cancel.top=JSON.parse(localStorage.getItem('L'))+'%';
+        cancel.left=JSON.parse(localStorage.getItem('T'))+'%';
+        s=localStorage.getItem('T')*99;
+        console.log(s);
+        setTimeout(()=>{life_1();},s)    
+    }
+function  create_CORDONATION_cancel()
+    {
+        life.top=JSON.parse(localStorage.getItem('T'))+'%';
+        life.left=JSON.parse(localStorage.getItem('L'))+'%';
+        s=localStorage.getItem('L')*99;
+        console.log(s);
+        setTimeout(()=>{can_cel();},s)    
+    }
 
-    function create_CORDONATION()
-        {
-            localStorage.setItem('L',Math.random()*100-1);
-            localStorage.setItem('T',Math.random()*100-1);
-            cordonation.top=JSON.parse(localStorage.getItem('T'))+'%';
-            cordonation.left=JSON.parse(localStorage.getItem('L'))+'%';
-        }   
-    function create_point()
-        {     
-            create_CORDONATION();
-            localStorage.setItem('Score',score=0)
-            $('.point').first().remove();
-            new pointer(cordonation);  
-            create_CORDONATION_life();      
-        }
-    $(document).on('click','.point',function()
-        {    
-            localStorage.setItem('Score',score++)
-            new pointer(cordonation);     
-            create_CORDONATION();  
-            $('.point').first().remove();
-        })
-
+function create_CORDONATION()
+    {
+        localStorage.setItem('L',Math.random()*98);
+        localStorage.setItem('T',Math.random()*98);
+        cordonation.top=JSON.parse(localStorage.getItem('T'))+'%';
+        cordonation.left=JSON.parse(localStorage.getItem('L'))+'%';
+    }   
+function create_point()
+    {     
+        create_CORDONATION();
+        localStorage.setItem('Score',score=0)
+        $('.point').first().remove();
+        new pointer(cordonation);  
+        create_CORDONATION_life();  
+        create_CORDONATION_cancel();    
+    }
+$(document).on('click','.point',function()
+    {    
+        localStorage.setItem('Score',score++)
+        new pointer(cordonation);     
+        create_CORDONATION();  
+        $('.point').first().remove();
+    })
 $('#start').on('click',function()
 {    
 i=10;

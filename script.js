61fe0cda3478f8a6bb22d15d710cd70fb79e03ab
Puiu Dumitru99 <puiudumitru99@gmail.com>
2023-05-25 15:48:32 +0300

    V2.1

--- script.js
+++ script.js
@@ -27,69 +27,65 @@ let life=
     }
 let cancel=
     {
-        clas:"cancel"
+        clas:"cancel",
+        tag:"span"
     }
     life._proto_=cordonation;
-    cancel._proto_=life;
-
-function life_1()
+    cancel._proto_=cordonation;
+function life_1() //Function for creation new heart wich add some secunds for continue game
     {
         new pointer(life);
     }
-    function can_cel()
+function can_cel() //Function for creation new cancel wich stope the game
     {
         new pointer(cancel);
     }
-    $(document).on('click','.cancel',function()
-    {
-        $('.cancel').first().remove();
-        $(this).remove();
-    })
-$(document).on('click','.heart',function()
+$(document).on('click','.heart',function() //click for heart  add some secunds for continue game
     {           
-        if(i===0)
+        if(i===0) // if i(time(secunds)) are = 0 the game will be finish and the heart will be hide
         { 
             console.warn("Finish");
             $(this).hide();     
         }
-        else if(i<=5)
+        else if(i<=5) //if i(time(secunds)) are <=5 then time will be add 5s
         {
             life.tC="+5";
-            i=i+5;
+            i=i+5+1;
             $(this).hide();
             create_CORDONATION_life();     
         }
-        else if(i>=7)
+        else if(i>=6) //if i(time(secunds)) are <=7 then time will be add 3s
         {
             life.tC="+3";
-            i=i+3;
+            i=i+3+1;
             $(this).hide();
             create_CORDONATION_life(); 
         }
         else
-        {
+        { // else game over
             $(this).hide();
             console.warn('Game over O_O');
             create_CORDONATION_life(); 
         }
     })
-function create_CORDONATION_life()
+function create_CORDONATION_life() // function 
     {
-        cancel.top=JSON.parse(localStorage.getItem('L'))+'%';
-        cancel.left=JSON.parse(localStorage.getItem('T'))+'%';
+        life.top=JSON.parse(localStorage.getItem('L'))+'%';
+        life.left=JSON.parse(localStorage.getItem('T'))+'%';
         s=localStorage.getItem('T')*99;
         console.log(s);
         setTimeout(()=>{life_1();},s)    
     }
 function  create_CORDONATION_cancel()
     {
-        life.top=JSON.parse(localStorage.getItem('T'))+'%';
-        life.left=JSON.parse(localStorage.getItem('L'))+'%';
-        s=localStorage.getItem('L')*99;
+        cancel.top=localStorage.getItem('T')+1+'%';
+        cancel.left=localStorage.getItem('L')+1+'%';
+        s=localStorage.getItem('L')*85;
         console.log(s);
-        setTimeout(()=>{can_cel();},s)    
+        setTimeout(()=>{
+            can_cel();
+        },s)    
     }
-
 function create_CORDONATION()
     {
         localStorage.setItem('L',Math.random()*98);
@@ -106,6 +102,34 @@ function create_point()
         create_CORDONATION_life();  
         create_CORDONATION_cancel();    
     }
+function finish_game()
+    {
+        alert("Finish,"+ localStorage.getItem('User Name')+" Your score is :="+localStorage.getItem('Score'))
+            localStorage.setItem('Score',0);
+            time.textContent="Time";           
+            $('.point').remove();
+            $('.heart').remove();
+            $('.cancel').remove();
+            $('#start').show();  
+    }
+$(document).on('click','.cancel',function()
+    {    
+        $(this).remove();
+        
+            console.warn("You touch me")    
+        if($('.cancel').length>=2)
+            {                        
+                $('span').first().remove();            
+            }
+            else 
+            {
+            finish_game();
+                i=0;
+            }
+            create_CORDONATION_cancel();
+           }
+        
+    )
 $(document).on('click','.point',function()
     {    
         localStorage.setItem('Score',score++)
@@ -114,8 +138,7 @@ $(document).on('click','.point',function()
         $('.point').first().remove();
     })
 $('#start').on('click',function()
-{    
-i=10;
+{    i=10;
    let time_action = setInterval(()=>
     {
         if(i>0)
@@ -124,27 +147,22 @@ i=10;
             i--;
         }
         else 
-        {  
-            alert("Finish,"+ localStorage.getItem('User Name')+" Your score is :="+localStorage.getItem('Score'))
-            localStorage.setItem('Score',0);
-            time.textContent="Time"
+        {   
+            location.reload(true);
+            finish_game();
             clearInterval(time_action);
-            $('.point').first().remove();
-            $('.heart').first().remove();
-            $('#start').show();  
         }
     },1000) 
     $(this).hide();
 });
-
-//log();
 function log()
 {
+alert("GAME RULES: It`s a simple game,press 'Start' to begin. Goal of the game :collect as many POINTS as possible IN TEN SECONDS")
 let user=prompt('Insert your name');
 if(user=="")
 {
     user="Un fuflic";
 }
 localStorage.setItem('User Name',user+" O_O ");
-alert("GAME RULES: It`s a simple game,press 'Start' to begin. Goal of the game :collect as many POINTS as possible IN TEN SECONDS")
 }
+log();
